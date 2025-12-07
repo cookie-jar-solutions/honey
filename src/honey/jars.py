47149e9f2b38a84aeadb9b20ba64f4116e9120d4
Anthony Saieva <anthony@cookie-jar-solutions.com>
2025-12-07 15:40:26 -0500

    optional dotenv

--- src/honey/jars.py
+++ src/honey/jars.py
@@ -8,12 +8,8 @@ import contextvars
 from typing import Optional, List, Dict, Any
 from abc import ABC, abstractmethod
 
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-except ImportError:
-    # dotenv is optional
-    pass
+from dotenv import load_dotenv
+load_dotenv()
 
 # Context variables to track active jars (separate for sync and async)
 _sync_jar: contextvars.ContextVar[Optional['Jar']] = contextvars.ContextVar(
